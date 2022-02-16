c48e724d98dac442d030bd08df4a683ad5836ed6
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-02-16 12:24:18 -0500

    updating profile element to use variables

--- react-version/src/components/Profile.js
+++ react-version/src/components/Profile.js
@@ -1,12 +1,15 @@
 function Profile() {
+  const firstName = 'Ted';
+  const lastName = 'Mosby';
+  const profilePic = '/profile-hex.png';
   return (
     <aside>
       <div className="profile">
-        <img className="profile__image" src="./profile-hex.png" />
+        <img className="profile__image" src={profilePic} />
       </div>
       <br />
       <div className="profile__name">
-        <h2><span className="profile--bold">Amy</span> Mansell</h2>
+        <h2><span className="profile--bold">{firstName}</span> {lastName}</h2>
       </div>
     </aside>
   );
