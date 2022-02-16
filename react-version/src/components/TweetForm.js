a93fc7f3ac69067e6cc511702d1bc2e86dd8520e
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-02-16 12:51:20 -0500

    preventing submitTweet form event

--- react-version/src/components/TweetForm.js
+++ react-version/src/components/TweetForm.js
@@ -2,11 +2,16 @@ function TweetForm() {
 
   const hummingText = 'What are you humming about?';
 
+  const formSubmit = e => {
+    e.preventDefault();
+  }
+
+
   return (
       <section className="newtweet">
-        <form method="post" action="/tweets" className="newtweet__form">
+        <form method="post" action="/tweets" className="newtweet__form" onSubmit={formSubmit}>
           <textarea className="form__textarea" name="text" placeholder={hummingText}></textarea>
-          <input type="submit" value="Tweet" className="form__input" />
+          <input type="submit" value="Tweet" className="form__input"/>
           <span className="form__counter">140</span>
         </form>
       </section>
